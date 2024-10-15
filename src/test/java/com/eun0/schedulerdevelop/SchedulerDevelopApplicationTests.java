a13bb3a1a362a6c21cbe080595e56396a5a65758
eun0 <shinely12@gmail.com>
2024-10-15 22:01:39 +0900

    init commit

---
+++ src/test/java/com/eun0/schedulerdevelop/SchedulerDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eun0.schedulerdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
