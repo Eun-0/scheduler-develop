1259f86980ca47ec50eadc2e46e6190ccd125043
eun0 <shinely12@gmail.com>
2024-10-29 12:06:54 +0900

    Refactor: @RequiredArgsConstructor 사용 및 코드 정리, 불필요한 주석 삭제

--- src/main/java/com/eun0/schedulerdevelop/controller/ScheduleController.java
+++ src/main/java/com/eun0/schedulerdevelop/controller/ScheduleController.java
@@ -6,31 +6,26 @@ import com.eun0.schedulerdevelop.dto.schedule.ScheduleResponse;
 import com.eun0.schedulerdevelop.dto.schedule.ScheduleUpdateRequest;
 import com.eun0.schedulerdevelop.service.ScheduleService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/schedules")
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
-    public ScheduleController(ScheduleService scheduleService) {
-        this.scheduleService = scheduleService;
-    }
-
-    // CREATE
     @PostMapping("")
     public ScheduleResponse createSchedule(@RequestBody @Valid ScheduleCreateRequest requestDto) {
         return scheduleService.createSchedule(requestDto);
     }
 
-    // READ
     @GetMapping("/{scheduleId}")
     public ScheduleResponse readScheduleById(@PathVariable Long scheduleId) {
         return scheduleService.readScheduleById(scheduleId);
     }
 
-    // READ AS PAGE SIZE
     @GetMapping("")
     public Page<SchedulePagingResponse> readSchedulesAsPageSize(
             @RequestParam(value = "page", defaultValue = "0") int page,
@@ -39,13 +34,11 @@ public class ScheduleController {
         return scheduleService.readSchedulesAsPageSize(page, size);
     }
 
-    // UPDATE
     @PutMapping("/{scheduleId}")
     public ScheduleResponse updateSchedule(@PathVariable Long scheduleId, @RequestBody @Valid ScheduleUpdateRequest requestDto) {
         return scheduleService.updateSchedule(scheduleId, requestDto);
     }
 
-    // DELETE
     @DeleteMapping("/{scheduleId}")
     public void deleteSchedule(@PathVariable Long scheduleId) {
         scheduleService.deleteSchedule(scheduleId);
