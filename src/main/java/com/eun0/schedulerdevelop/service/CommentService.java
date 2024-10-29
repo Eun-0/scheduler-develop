653ca69e70d9ece952bc9b5a2c153edc38e1ab87
eun0 <shinely12@gmail.com>
2024-10-29 19:28:33 +0900

    Fix: 의존성 주입, 메서드들의 매개변수 수정, 예외 처리 추가, 불필요한 주석 삭제
    - User와 Comment 사이의 연관관계 설정으로 인한, UserRepository 의존성 주입
    - Schedule과 Comment 사이의 연관관계 설정으로 인한, ScheduleRepository 의존성 주입
    - 메서드들의 매개변수 수정에 따른 메서드 로직 수정

--- src/main/java/com/eun0/schedulerdevelop/service/CommentService.java
+++ src/main/java/com/eun0/schedulerdevelop/service/CommentService.java
@@ -1,10 +1,13 @@
 package com.eun0.schedulerdevelop.service;
 
-import com.eun0.schedulerdevelop.dto.comment.CommentCreateRequest;
+import com.eun0.schedulerdevelop.dto.comment.CommentRequest;
 import com.eun0.schedulerdevelop.dto.comment.CommentResponse;
-import com.eun0.schedulerdevelop.dto.comment.CommentUpdateRequest;
 import com.eun0.schedulerdevelop.entity.Comment;
+import com.eun0.schedulerdevelop.entity.Schedule;
+import com.eun0.schedulerdevelop.entity.User;
 import com.eun0.schedulerdevelop.repository.CommentRepository;
+import com.eun0.schedulerdevelop.repository.ScheduleRepository;
+import com.eun0.schedulerdevelop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,41 +17,40 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+    UserRepository userRepository;
+    ScheduleRepository scheduleRepository;
     CommentRepository commentRepository;
 
     @Transactional
-    public CommentResponse createComment(CommentCreateRequest requestDto) {
-        // RequestDTO -> Entity
-        Comment comment = requestDto.toEntity();
+    public CommentResponse createComment(Long userId, Long scheduleId, CommentRequest requestDto) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 유저가 DB에 존재하지 않습니다."));
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(() -> new IllegalArgumentException("해당 일정이 DB에 존재하지 않습니다."));
 
-        // DB 저장
-        Comment savedComment = commentRepository.save(comment);
+        Comment savedComment = commentRepository.save(Comment.from(requestDto, user, schedule));
 
-        // Entity -> ResponseDTO
         return CommentResponse.from(savedComment);
     }
 
     @Transactional
-    public List<CommentResponse> readAllComments() {
-        return commentRepository.findAll().stream().map(CommentResponse::from).toList();
+    public List<CommentResponse> readAllCommentsByScheduleId(Long scheduleId) {
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(() -> new IllegalArgumentException("해당 일정이 DB에 존재하지 않습니다."));
+        List<Comment> comments = schedule.getCommentList();
+        return comments.stream().map(CommentResponse::from).toList();
     }
 
     @Transactional
-    public CommentResponse updateComment(Long id, CommentUpdateRequest requestDto) {
-        // 해당 댓글이 DB에 존재하는지 확인
-        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 DB에 존재하지 않습니다."));
+    public CommentResponse updateComment(Long scheduleId, Long commentId, CommentRequest requestDto) {
+        scheduleRepository.findById(scheduleId).orElseThrow(() -> new IllegalArgumentException("해당 일정이 DB에 존재하지 않습니다."));
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 DB에 존재하지 않습니다."));
 
-        // RequestDTO -> Entity
         comment.update(requestDto.getContent());
-
-        // Entity -> ResponseDTO
         return CommentResponse.from(comment);
     }
 
     @Transactional
-    public void deleteComment(Long id) {
-        // 해당 댓글이 DB에 존재하는지 확인
-        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 DB에 존재하지 않습니다."));
+    public void deleteComment(Long scheduleId, Long commentId) {
+        scheduleRepository.findById(scheduleId).orElseThrow(() -> new IllegalArgumentException("해당 일정이 DB에 존재하지 않습니다."));
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 DB에 존재하지 않습니다."));
 
         commentRepository.delete(comment);
     }
