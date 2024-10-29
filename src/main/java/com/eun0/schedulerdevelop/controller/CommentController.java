ddcf94daf00d6b1b4399662689a13349ffbc9d7d
eun0 <shinely12@gmail.com>
2024-10-29 15:53:36 +0900

    Refactor: @RequiredArgsConstructor 사용 및 코드 정리, 불필요한 주석 삭제

--- src/main/java/com/eun0/schedulerdevelop/controller/CommentController.java
+++ src/main/java/com/eun0/schedulerdevelop/controller/CommentController.java
@@ -5,38 +5,32 @@ import com.eun0.schedulerdevelop.dto.comment.CommentResponse;
 import com.eun0.schedulerdevelop.dto.comment.CommentUpdateRequest;
 import com.eun0.schedulerdevelop.service.CommentService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/schedules/{scheduleId}/comments")
 public class CommentController {
     private final CommentService commentService;
 
-    public CommentController(CommentService commentService) {
-        this.commentService = commentService;
-    }
-
-    // CREATE
     @PostMapping("")
     public CommentResponse createComment(@PathVariable Long scheduleId, @RequestBody @Valid CommentCreateRequest requestDto) {
         return commentService.createComment(requestDto);
     }
 
-    // READ
     @GetMapping("")
     public List<CommentResponse> readAllComments(@PathVariable Long scheduleId) {
         return commentService.readAllComments();
     }
 
-    // UPDATE
     @PutMapping("/{commentId}")
     public CommentResponse updateComment(@PathVariable Long scheduleId, @PathVariable Long commentId, @RequestBody @Valid CommentUpdateRequest requestDto) {
         return commentService.updateComment(commentId, requestDto);
     }
 
-    // DELETE
     @DeleteMapping("/{commentId}")
     public void deleteComment(@PathVariable Long scheduleId, @PathVariable Long commentId) {
         commentService.deleteComment(commentId);
