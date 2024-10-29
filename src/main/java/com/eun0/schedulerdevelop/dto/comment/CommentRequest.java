842795369eb5a8178ae3c94720f5c23e530a9b7d
eun0 <shinely12@gmail.com>
2024-10-29 17:27:11 +0900

    Fix: Comment의 RequestDto 병합

---
+++ src/main/java/com/eun0/schedulerdevelop/dto/comment/CommentRequest.java
@@ -0,0 +1,12 @@
+package com.eun0.schedulerdevelop.dto.comment;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class CommentRequest {
+    @NotNull(message = "내용은 Null일 수 없습니다.")
+    @Size(min=1, max=20, message = "댓글은 최소 1자에서 최대 20자까지 가능합니다.")
+    private String content;
+}
