02e59c1295dc9c25e48c1a922f17b8c3d7f16e33
eun0 <shinely12@gmail.com>
2024-10-29 12:25:43 +0900

    Fix: User와 Schedule 사이의 연관관계 설정으로 인한 writer 필드 삭제, toEntity() 메서드 수정

--- src/main/java/com/eun0/schedulerdevelop/dto/schedule/ScheduleCreateRequest.java
+++ src/main/java/com/eun0/schedulerdevelop/dto/schedule/ScheduleCreateRequest.java
@@ -6,11 +6,6 @@ import lombok.Getter;
 
 @Getter
 public class ScheduleCreateRequest {
-    @NotBlank(message = "작성자명은 공백일 수 없습니다.")
-    @Size(min=1, max=5, message = "작성자명은 최소 1자에서 최대 5자까지 가능합니다.")
-    @Pattern(regexp = "^[a-zA-Z가-힣0-9\\p{Punct}]*$", message = "작성자명은 공백을 제외한 영어, 한글, 숫자, 특수기호만 가능합니다.")
-    private String writer;
-
     @NotNull(message = "제목은 Null일 수 없습니다.")
     @Size(min=1, max=15, message = "제목은 최소 1자에서 최대 15자까지 가능합니다.")
     private String title;
@@ -20,6 +15,6 @@ public class ScheduleCreateRequest {
     private String content;
 
     public Schedule toEntity() {
-        return new Schedule(this.writer, this.title, this.content);
+        return new Schedule(this.title, this.content);
     }
 }
